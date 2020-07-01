198939d1e634bf2ca58d02520b59fb81444a2ea4
Blake Hudson <hudsonbl@oregonstate.edu>
2020-07-01 15:01:10 -0700

    changes

--- server/initglobals.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-command1=$()
-command2=$()
-command3=$()
-command4=$()
-echo "echo export MYSQL_DATABASE=medinfo"
-echo "echo export MYSQL_PASSWORD=gambit"
-echo "echo export MYSQL_USER=stroot"
-echo "echo export SECRET_KEY=gambit"
