e7ec27f6ef8f1d82703595b2895d5a1ff508a0b6
blake hudson <hudsonbl@oregonstate.edu>
2020-09-18 14:48:02 -0700

    Last one crossing fingers

--- client/src/components/homepage/Login.js
+++ client/src/components/homepage/Login.js
@@ -79,7 +79,7 @@ export default function SignIn() {
         const requestOptions = {
             method: 'POST',
             headers: {'Content-Type': 'application/json',
-                      'accept': 'application/json'},
+					  'accept': 'application/json'},
             body: JSON.stringify({
               email: email,
               password: password
